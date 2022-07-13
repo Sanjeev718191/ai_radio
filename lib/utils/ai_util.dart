e5fe458a18dd9b63e8bf78095ca2c5ade1c92723
Sanjeev <sanjeev718191@gmail.com>
2022-07-13 23:43:32 +0530

    first commit

---
+++ lib/utils/ai_util.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static Color primaryColor1 = Color.fromARGB(255, 225, 0, 192);
+  static Color primaryColor2 = Color.fromARGB(255, 33, 0, 212);
+}
